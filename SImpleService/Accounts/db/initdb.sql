47db9ffba192a7bcf5c9161c46a979bb166528d2
WenXiangSM <148532098+WenXiangSM@users.noreply.github.com>
2024-03-21 21:02:49 +0800

    change initdb.sql

--- SImpleService/Accounts/db/initdb.sql
+++ SImpleService/Accounts/db/initdb.sql
@@ -17,10 +17,10 @@ CREATE TABLE IF NOT EXISTS account (
 INSERT INTO account (name, email, password, role, allergies)
 VALUES ('John Doe', 'johndoe@email.com', 'password1', 0, NULL),
        ('Jane Smith', 'janesmith@email.com', 'password2', 0, NULL),
-       ('Michael Lee', 'mlee@email.com', 'password3', 0, NULl),
+       ('Michael Lee', 'mlee@email.com', 'password3', 0, NULL),
        ('Alice Brown', 'aliceb@email.com', 'password4', 1, '["1"]'),
        ('David Johnson', 'djohnson@email.com', 'password5', 1, '["7", "11"]'),
-       ('Dan White','dWhite@gmail.com','password6', 1, '["4", "8"]')
+       ('Dan White','dWhite@gmail.com','password6', 1, '["4", "8"]'),
        ('Sarah Williams', 'sarah@email.com', 'password7', 1, NULL),
        ('Robert Davis', 'robert@email.com', 'password8', 1, NULL),
        ('Emily Johnson', 'emily@email.com', 'password9', 1, NULL),
